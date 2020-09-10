127982644643b54e43ef7cabdb1bdbfae4120a51
poramin <614259039@webmail.npru.ac.th>
2020-09-10 12:19:04 +0700

    session1

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	
+	function __construct()
+	{
+		 parent::__construct();
+		 echo "My Constucter"."</br>";
+	}
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$data['name'] = "Poramin";
+		//$data['email'] = "poramin0013@gmail.com";
+		$data['student'] = array("614259039","poramin","jumphom","Software Engineering");
+		$this->load->view("hello_view",$data);
+		
+	}
+
+	public function show($name=null,$lastname=null)
+	{
+		echo " Hello ".$name. " ".$lastname;
+		$this->_display();
+	}
+	public function _display(){
+		echo "</br>"."Softwere Engineering";
 	}
 }
---
+++ application/views/hello_view.php
@@ -0,0 +1,7 @@
+<?php
+       // echo "Hello ".$name. " and Email; ".$email;
+       foreach($student as $std1){
+           echo " ".$std1."</br/r>";
+       }
+?>
+<center><h1>NPRU</h1></center>
